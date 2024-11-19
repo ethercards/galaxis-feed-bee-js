d79d0471a15328d2e52c5e9c92478f4eefd2d513
Ferenc Mucsi <ferenc.mucsi@gmail.com>
2024-11-19 11:05:55 +0100

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
 exports.buy = buy;
 exports.feed = feed;
 exports.upload = upload;
@@ -41,52 +40,49 @@ function upload(file) {
         return result;
     });
 }
-const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    const args = process.argv.slice(3); // Get command-line arguments
-    console.log('Arguments:', args);
-    if (args.length > 0) {
-        switch (args[0]) {
-            case 'help':
-            case '?':
-                console.log('Usage:');
-                console.log('  help or ?: Display this help message');
-                console.log('  buy <amount> <depth>: Buy a new postage batch');
-                console.log('  upload <file-path>: Upload a file');
-                console.log('  feed <file-path> <topic>: Write to a feed');
-                break;
-            case 'buy':
-                if (args.length > 2) {
-                    return yield bee_plus_1.default.createPostageBatch(args[1], args[2]);
-                }
-                else {
-                    console.log('usage: buy <amount> <depth>');
-                }
-            case 'upload':
-                if (args.length > 1) {
-                    return yield bee_plus_1.default.upload(args[1]);
-                }
-                else {
-                    console.log("usage: upload <file-path>");
-                }
-                break;
-            case 'feed':
-                if (args.length > 2) {
-                    try {
-                        yield bee_plus_1.default.writeFeed(args[1], args[2]);
-                    }
-                    catch (error) {
-                        console.error('Error:', error);
-                        throw error;
-                    }
-                }
-                else {
-                    console.log("usage: feed <file-path> <topic>");
-                }
-                break;
-            default:
-                console.log('Invalid command:', args[0]);
-        }
-    }
-});
-exports.main = main;
+// export const main = async () => {
+//     const args = process.argv.slice(3); // Get command-line arguments
+//     console.log('Arguments:', args);
+//     if (args.length > 0) {
+//         switch (args[0]) {
+//             case 'help':
+//             case '?':
+//                 console.log('Usage:');
+//                 console.log('  help or ?: Display this help message');
+//                 console.log('  buy <amount> <depth>: Buy a new postage batch');
+//                 console.log('  upload <file-path>: Upload a file');
+//                 console.log('  feed <file-path> <topic>: Write to a feed');
+//                 break;
+//             case 'buy':
+//                 if (args.length > 2) {
+//                     return await beePlus.createPostageBatch(args[1], args[2]);
+//                 } else {
+//                     console.log('usage: buy <amount> <depth>');
+//                 }
+//             case 'upload':
+//                 if (args.length > 1) {
+//                     return await beePlus.upload(args[1]);
+//                 }
+//                 else {
+//                     console.log("usage: upload <file-path>");
+//                 }
+//                 break;
+//             case 'feed':
+//                 if (args.length > 2) {
+//                     try {
+//                         await beePlus.writeFeed(args[1], args[2]);
+//                     } catch (error) {
+//                         console.error('Error:', error);
+//                         throw error;
+//                     }
+//                 }
+//                 else {
+//                     console.log("usage: feed <file-path> <topic>");
+//                 }
+//                 break;
+//             default:
+//                 console.log('Invalid command:', args[0]);
+//         }
+//     }
+// }
 //main();
--- src/index.ts
+++ src/index.ts
@@ -24,53 +24,53 @@ export async function upload(file: string): Promise<UploadResultWithCid> {
     return result;
 }
 
-export const main = async () => {
+// export const main = async () => {
 
-    const args = process.argv.slice(3); // Get command-line arguments
-    console.log('Arguments:', args);
+//     const args = process.argv.slice(3); // Get command-line arguments
+//     console.log('Arguments:', args);
 
-    if (args.length > 0) {
-        switch (args[0]) {
-            case 'help':
-            case '?':
-                console.log('Usage:');
-                console.log('  help or ?: Display this help message');
-                console.log('  buy <amount> <depth>: Buy a new postage batch');
-                console.log('  upload <file-path>: Upload a file');
-                console.log('  feed <file-path> <topic>: Write to a feed');
-                break;
-            case 'buy':
-                if (args.length > 2) {
-                    return await beePlus.createPostageBatch(args[1], args[2]);
-                } else {
-                    console.log('usage: buy <amount> <depth>');
-                }
+//     if (args.length > 0) {
+//         switch (args[0]) {
+//             case 'help':
+//             case '?':
+//                 console.log('Usage:');
+//                 console.log('  help or ?: Display this help message');
+//                 console.log('  buy <amount> <depth>: Buy a new postage batch');
+//                 console.log('  upload <file-path>: Upload a file');
+//                 console.log('  feed <file-path> <topic>: Write to a feed');
+//                 break;
+//             case 'buy':
+//                 if (args.length > 2) {
+//                     return await beePlus.createPostageBatch(args[1], args[2]);
+//                 } else {
+//                     console.log('usage: buy <amount> <depth>');
+//                 }
 
-            case 'upload':
-                if (args.length > 1) {
-                    return await beePlus.upload(args[1]);
-                }
-                else {
-                    console.log("usage: upload <file-path>");
-                }
-                break;
-            case 'feed':
-                if (args.length > 2) {
-                    try {
-                        await beePlus.writeFeed(args[1], args[2]);
-                    } catch (error) {
-                        console.error('Error:', error);
-                        throw error;
-                    }
-                }
-                else {
-                    console.log("usage: feed <file-path> <topic>");
-                }
-                break;
-            default:
-                console.log('Invalid command:', args[0]);
-        }
-    }
-}
+//             case 'upload':
+//                 if (args.length > 1) {
+//                     return await beePlus.upload(args[1]);
+//                 }
+//                 else {
+//                     console.log("usage: upload <file-path>");
+//                 }
+//                 break;
+//             case 'feed':
+//                 if (args.length > 2) {
+//                     try {
+//                         await beePlus.writeFeed(args[1], args[2]);
+//                     } catch (error) {
+//                         console.error('Error:', error);
+//                         throw error;
+//                     }
+//                 }
+//                 else {
+//                     console.log("usage: feed <file-path> <topic>");
+//                 }
+//                 break;
+//             default:
+//                 console.log('Invalid command:', args[0]);
+//         }
+//     }
+// }
 
 //main();
